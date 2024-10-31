4aa31a4ac57e2fd03eaafe792b91e09df28e0849
agayan <a.agayan95@mail.ru>
2024-10-31 22:40:31 +0400

    fix: test исправлен

--- main-service/src/test/User/UserServiceTest.java
+++ main-service/src/test/User/UserServiceTest.java
@@ -2,44 +2,50 @@ package User;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.practicum.user.dto.UserDto;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
 import ru.practicum.user.mappers.UserMapper;
 import ru.practicum.user.model.User;
 import ru.practicum.user.repository.UserRepository;
-import ru.practicum.user.service.UserService;
+import ru.practicum.user.service.UserServiceImpl;
 
-
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
 
+    private MockedStatic<UserMapper> userMapperMock;
+
     @InjectMocks
-    private UserService userService;
+    private UserServiceImpl userService;
 
     private UserDto userDto;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
         userDto = new UserDto();
-        userDto.setName("John Doe");
-        userDto.setEmail("johndoe@example.com");
+        userDto.setName("TestName");
+        userDto.setEmail("testEmail@mail.ru");
+
+        userMapperMock = Mockito.mockStatic(UserMapper.class);
     }
 
     @Test
     public void testAddUserSuccess() {
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        when(UserMapper.mapToUser(any(UserDto.class))).thenReturn(user);
 
-        User user = UserMapper.mapToUser(userDto);
         when(userRepository.save(any(User.class))).thenReturn(user);
+
+        when(UserMapper.mapToUserDto(any(User.class))).thenReturn(userDto);
 
         UserDto createUser = userService.addUser(userDto);
 
@@ -47,6 +53,5 @@ public class UserServiceTest {
         assertEquals(userDto.getName(), createUser.getName());
         assertEquals(userDto.getEmail(), createUser.getEmail());
         verify(userRepository, times(1)).save(any(User.class));
-
     }
 }
